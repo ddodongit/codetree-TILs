30afd40df660b5590d88cd5d8262b3a91aa96486
ddodongit <44135024+ddodongit@users.noreply.github.com>
2025-02-04 17:33:29 +0900

    Title: 왔다 갔던 구역 2, Time: 96ms, Memory: 9MB, Status: Wrong Answer - CodeTree

---
+++ 250204/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -0,0 +1,37 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        // Please write your code here.
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[] arr = new int[2000];                
+
+        int now = 1000;
+        int N = Integer.parseInt(br.readLine());
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+        for(int i=0; i<N; i++){
+            st = new StringTokenizer(br.readLine());
+            int offset = Integer.parseInt(st.nextToken());
+            char ch = st.nextToken().charAt(0);
+
+            int dir = ch == 'R' ? 1 : -1;
+            for(int j=0; j<offset; j++){
+                arr[now]++;
+                min = Integer.min(now, min);
+                max = Integer.max(now, max);
+                now += dir;
+            }
+        }
+        int count = 0;
+        for(int i=min; i<=max; i++){
+            if(arr[i] >=2) count++;
+        }
+
+        System.out.println(count);
+        
+    }
+}
